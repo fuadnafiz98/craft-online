8ef08efcec377cb004659b2d14583cbd263ddcf6
fuadnafiz98 <43469396+fuadnafiz98@users.noreply.github.com>
2019-09-30 11:27:09 +0600

    back

--- find_id.js
+++ find_id.js
@@ -14,8 +14,8 @@ function own(elem) {
                 // *
                 localStorage.setItem("cat", product.catagory);
                 console.log('kkkkk');
-                location.href = "./views/products.html";
-//                  location.href = "../views/products.html";
+//                 location.href = "./views/products.html";
+                 location.href = "../views/products.html";
                 //     document.getElementById('total').innerHTML = ' ';
                 //     console.log(window.location);
             }
